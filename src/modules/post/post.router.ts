1b59316f05c7d204f78ca2b72bfaf5138fd3543d
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-02-05 21:57:24 +0600

    feat: initiates role based authentication

--- src/modules/post/post.router.ts
+++ src/modules/post/post.router.ts
@@ -1,9 +1,75 @@
-import express, { Router } from "express"
+import express, { NextFunction, Request, Response, Router } from "express"
 import { PostController } from "./post.controller";
+import { auth as betterAuth } from "../../lib/auth";
 
 const router = express.Router();
 
-router.post("/", PostController.createPost);
+export enum UserRole {
+    USER = "USER",
+    ADMIN = "ADMIN"
+}
+
+declare global {
+    namespace Express {
+        interface Request {
+            user?: {
+                id: string;
+                email: string;
+                name: string;
+                role: string;
+                emailVerified: boolean;
+            }
+        }
+    }
+}
+
+const auth = (...roles: UserRole[]) => {
+    return async (req: Request, res: Response, next: NextFunction) =>{
+        
+        // get user session
+        const session = await betterAuth.api.getSession({
+            headers: req.headers as any
+        })
+
+        if(!session) {
+            return res.status(401).json({
+                success: false,
+                message: "You are not Authorized",
+            })
+        }
+
+        if(!session.user.emailVerified) {
+            res.status(403).json({
+                success: false,
+                message: "Email verification required. Please verify your email",
+            })
+        }
+
+        req.user = {
+            id: session.user.id,
+            email: session.user.email,
+            name: session.user.name,
+            role: session.user.role as string,
+            emailVerified: session.user.emailVerified,
+        }
+
+        
+
+        console.log(session );
+
+        if(roles.length && !roles.includes(req.user.role as UserRole)) {
+            return res.status(403).json({
+                success: false,
+                message: "Forbidden! You don't have permission to access this resources!"
+            })
+        }
+
+        next();
+    }
+}
+
+
+router.post("/", auth(UserRole.USER), PostController.createPost);
 
 
 
