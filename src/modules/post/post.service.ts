8e74a36c5ef38ce14424487facd49007c68408b4
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-02-07 21:57:41 +0600

    applies OR, HAS when searching

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -2,30 +2,45 @@
 import { Post } from "../../../generated/prisma/client";
 import { prisma } from "../../lib/prisma";
 
-const getAllPost = async (payload : { search: string | undefined}) => {
+const getAllPost = async (payload: { search: string | undefined }) => {
     const result = await prisma.post.findMany({
         where: {
-            title : {
-                contains: payload.search as string,
-                mode: "insensitive"
-            }
-        }
+            OR: [
+                {
+                    title: {
+                        contains: payload.search as string,
+                        mode: "insensitive",
+                    },
+                },
+                
+                {
+                    content: {
+                        contains: payload.search as string,
+                        mode: "insensitive",
+                    },
+                },
+                {
+                    tags: {
+                        has: payload.search as string
+                    }
+                }
+            ],
+        },
     });
-    return result ;
-}
+    return result;
+};
 
-const createPost = async (data :Omit<Post, "id" | "createdAt" | "updatedAt" | "authorId">, userId: string)=>{
+const createPost = async (data: Omit<Post, "id" | "createdAt" | "updatedAt" | "authorId">, userId: string) => {
     const result = await prisma.post.create({
         data: {
             ...data,
-            authorId: userId
-        }
-    })
-    return result ;
-}
-
+            authorId: userId,
+        },
+    });
+    return result;
+};
 
 export const postService = {
     createPost,
-    getAllPost
-}
+    getAllPost,
+};
