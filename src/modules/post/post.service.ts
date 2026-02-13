00b1f32f9b6756875d39ef4483aea3bcbf6596d6
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-02-13 23:28:39 +0600

    finds post's comments and replies

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,4 +1,4 @@
-import { Post, PostStatus } from "../../generated/prisma/client";
+import { CommentStatus, Post, PostStatus } from "../../generated/prisma/client";
 import { PostWhereInput } from "../../generated/prisma/models";
 import { prisma } from "../../lib/prisma";
 
@@ -124,6 +124,28 @@ const getPostById = async (postId: string) => {
             where: {
                 id: postId,
             },
+            include: {
+                comments: {
+                    where: {
+                        parentId: null,
+                        status: CommentStatus.APPROVED
+                    },
+                    include: {
+                        replies: {
+                            where : {
+                                status: CommentStatus.APPROVED
+                            },
+                            include: {
+                                replies: {
+                                    where: {
+                                        status: CommentStatus.APPROVED
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         });
 
         return postData
