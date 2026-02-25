a352532e3b3e08f9064765bfd4fd9e3220844074
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-02-26 00:48:48 +0600

    refactor: moves cors middleware to the top of app.ts file

--- src/app.ts
+++ src/app.ts
@@ -8,16 +8,18 @@ import errorHandler from "./middleware/globalErrorHandler";
 import { notFound } from "./middleware/notFound";
 
 const app : Application = express();
-app.all("/api/auth/*splat", toNodeHandler(auth));
-
 app.use(cors({
     origin: process.env.APP_URL || "http://localhost:4000",
     credentials: true
 }))
+app.all("/api/auth/*splat", toNodeHandler(auth));
+
 app.use(express.json());
 
 app.use("/posts", postRouter);
 app.use("/comments", commentRouter);
+
+console.log("APP_URL:", process.env.APP_URL)
 
 app.get("/", (req, res)=>{
     res.send("Hello World")
