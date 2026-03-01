089c640fd57424998c01a76993f654725c9888a9
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-03-02 00:30:36 +0600

    just refactors emailVerified field

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -40,7 +40,7 @@ export const auth = betterAuth({
     emailAndPassword: {
         enabled: true,
         autoSignIn: false,
-        requireEmailVerification: true,
+        requireEmailVerification: true, 
     },
     emailVerification: {
         sendOnSignUp: true,
